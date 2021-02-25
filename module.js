89836814412286cc2fb2f68c4b15cee27cd92b88
Divyam <divyamj980fficial@gmail.com>
2021-02-25 13:44:06 +0530

    auth changes in module

--- module.js
+++ module.js
@@ -1,56 +1,56 @@
 // NPM module 
-// const color = require('cli-color')
-// console.log(color.green('Hello Node js'))
+const color = require('cli-color')
+console.log(color.green('Hello Node js'))
 
 // Local module
-// const auth = require('./auth')
+const auth = require('./auth')
 
-// auth.register('Divyam Juyal')
-// auth.login('Divyam Juyal', 'secret')
+auth.register('Divyam Juyal')
+auth.login('Divyam Juyal', 'secret')
 
 // Core modules
 const path = require('path')
 
 
 // dirname 
-// console.log('Folder name: ', path.dirname(__filename))
+console.log('Folder name: ', path.dirname(__filename))
 
 // filename
-// console.log('File name: ', path.basename(__filename))
+console.log('File name: ', path.basename(__filename))
 
 // Extension
-// console.log('Ext name: ', path.extname(__filename))
+console.log('Ext name: ', path.extname(__filename))
 
 // parse 
-// console.log('Parse : ', path.parse(__filename))
+console.log('Parse : ', path.parse(__filename))
 
 // Join 
-// console.log('Join : ', path.join(__dirname, 'order', 'app.js'))
+console.log('Join : ', path.join(__dirname, 'order', 'app.js'))
 
 // File system 
 const fs = require('fs')
 // Make a directory 
-// fs.mkdir(path.join(__dirname, '/test'), (err) => {
-//     if(err) {
-//         console.log(err)
-//         return
-//     }
-//     console.log('Folder created...')
-// })
+fs.mkdir(path.join(__dirname, '/test'), (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Folder created...')
+})
 
 // Create a file
-// fs.writeFile(path.join(__dirname, 'test', 'test.txt'), 'Hello Node\n', (err) => {
-//     if(err) {
-//         throw err
-//     }
-//     fs.appendFile(path.join(__dirname, 'test', 'test.txt'), 'More data', (err) => {
-//         if(err) {
-//             throw err
-//         } 
-//         console.log('Data added...') 
-//     })
-//     console.log('File created...')
-// })
+fs.writeFile(path.join(__dirname, 'test', 'test.txt'), 'Hello Node\n', (err) => {
+    if(err) {
+        throw err
+    }
+    fs.appendFile(path.join(__dirname, 'test', 'test.txt'), 'More data', (err) => {
+        if(err) {
+            throw err
+        } 
+        console.log('Data added...') 
+    })
+    console.log('File created...')
+})
 
 // Read a file 
 fs.readFile(path.join(__dirname, 'test', 'test.txt'), 'utf-8', (err, data) => {
@@ -84,40 +84,40 @@ fs.readFile(path.join(__dirname, 'test', 'test.txt'), 'utf-8', (err, data) => {
 // Os module 
 const os = require('os')
 
-// console.log('Os type: ', os.type())
+console.log('Os type: ', os.type())
 
-// console.log('Os platform: ', os.platform()) 
+console.log('Os platform: ', os.platform()) 
 
-// console.log('Cpu architecture: ', os.arch())
+console.log('Cpu architecture: ', os.arch())
 
-// console.log('Cpu details: ', os.cpus())
+console.log('Cpu details: ', os.cpus())
 
-// console.log('Free memory : ', os.freemem())
+console.log('Free memory : ', os.freemem())
 
-// console.log('Total memory : ', os.totalmem())
+console.log('Total memory : ', os.totalmem())
 
-// console.log('Uptime : ', os.uptime())
+console.log('Uptime : ', os.uptime())
 
 
 // Events module 
 const Emitter = require('events')
 
-// const myEmitter = new Emitter()
+const myEmitter = new Emitter()
 
-// myEmitter.on('somename', (data) => {
-//     console.log(data)
-// })
+myEmitter.on('somename', (data) => {
+    console.log(data)
+})
 
-// myEmitter.emit('somename', {
-//     name: 'Rakesh'
-// })
+myEmitter.emit('somename', {
+    name: 'Divyam'
+})
 
-// class Auth extends Emitter{
-//     register(username) {
-//         console.log('Registered successfully...')
-//         this.emit('registered', username)
-//     }
-// }
+class Auth extends Emitter{
+    register(username) {
+        console.log('Registered successfully...')
+        this.emit('registered', username)
+    }
+}
 
 // const auth = new Auth()
 
@@ -134,7 +134,7 @@ const Emitter = require('events')
 // })
 
 
-// auth.register('codersGyan')
+// auth.register('Divyam Juyal')
 
 
 
